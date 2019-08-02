e956f1e1f97fc0a2dcf5d4ee0f0ff5ce790055fc
xiankun.jiang <xiankun.jiang@aorise.org>
2019-08-02 14:18:43 +0800

    卷积，旋转180度

--- src/main/java/com/example/idu/ConvnUtils.java
+++ src/main/java/com/example/idu/ConvnUtils.java
@@ -6,6 +6,63 @@ package com.example.idu;
  * @Date: 2019/7/31 10:08
  */
 public class ConvnUtils {
+    public static double[][] cloneMatrix(final double[][] matrix) {
+        final int m = matrix.length;
+        int n = matrix[0].length;
+        final double[][] outMatrix = new double[m][n];
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                outMatrix[i][j] = matrix[i][j];
+        return outMatrix;
+    }
+
+    /**
+     * 对矩阵进行180度旋转,是在matrix的副本上复制，不会对原来的矩阵进行修改
+     *
+     * @param matrix
+     */
+    public static double[][] rot180(double[][] matrix) {
+        matrix = cloneMatrix(matrix);
+        int m = matrix.length;
+        int n = matrix[0].length;
+        // 按列对称进行交换
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                double tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = tmp;
+            }
+        }
+        // 按行对称进行交换
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < m / 2; i++) {
+                double tmp = matrix[i][j];
+                matrix[i][j] = matrix[m - 1 - i][j];
+                matrix[m - 1 - i][j] = tmp;
+            }
+        }
+        return matrix;
+    }
+
+    /**
+     * 克罗内克积,对矩阵进行扩展
+     */
+    public static double[][] kronecker(final double[][] matrix, int x) {
+        final int m = matrix.length;
+        int n = matrix[0].length;
+        final double[][] outMatrix = new double[m * x][n * x];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int ki = i * x; ki < (i + 1) * x; ki++) {
+                    for (int kj = j * x; kj < (j + 1) * x; kj++) {
+                        outMatrix[ki][kj] = matrix[i][j];
+                    }
+                }
+            }
+        }
+        return outMatrix;
+    }
+
     /**
      * @Description: Full模式卷积
      * @@param: [matrix, kernel]
@@ -35,7 +92,7 @@ public class ConvnUtils {
      * @date 2018/12/5  14:50
      */
     public static double[][] convnValid(final double[][] matrix, double[][] kernel) {
-        //kernel = rot180(kernel);
+        kernel = rot180(kernel);
         int m = matrix.length;
         int n = matrix[0].length;
         final int km = kernel.length;
@@ -99,13 +156,13 @@ public class ConvnUtils {
         double x = 1;
         for (int i = 0; i < 4; i++)
             for (int j = 0; j < 4; j++)
-                a[i][j] = x;
+                a[i][j] = x++;
         x = 1;
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
                 b[i][j] = x++;
 
-        double[][] c = convnValid(a,b);
+        double[][] c = convnValid(a, b);
         System.out.println("");
     }
 
