27c0ab76aba401779f4d78e3578b76197756c191
xiankun.jiang <xiankun.jiang@aorise.org>
2020-11-13 10:22:52 +0800

    nio学习 bio

--- src/main/java/com/example/nio/channel/Client.java
+++ src/main/java/com/example/nio/channel/Client.java
@@ -4,13 +4,24 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.*;
 import java.nio.channels.SocketChannel;
+import java.util.Scanner;
 import java.util.UUID;
 
 import static com.example.nio.Utils.byte2Byffer;
 import static com.example.nio.Utils.bytebuffer2ByteArray;
 
 /**
- * @Description:
+ * @Description: 这两个方法read()和readLine()都会读取对端发送过来的数据，如果无数据可读，就会阻塞直到有数据可读。或者到达流的末尾，这个时候分别返回-1和null。
+ * 这个特性使得编程非常方便也很高效。
+ * 但是这样也有一个问题，就是如何让程序从这两个方法的阻塞调用中返回。
+ * <p>
+ * 总结一下，有这么几个方法：
+ * 1）发送完后调用Socket的shutdownOutput()方法关闭输出流，这样对端的输入流上的read操作就会返回-1。
+ * 注意不能调用socket.getInputStream().close()。这样会导致socket被关闭。
+ * 当然如果不需要继续在socket上进行读操作，也可以直接关闭socket。
+ * 但是这个方法不能用于通信双方需要多次交互的情况。
+ * <p>
+ * 2）发送数据时，约定数据的首部固定字节数为数据长度。这样读取到这个长度的数据后，就不继续调用read方法。
  * @Author: xiankun.jiang
  * @Date: 2020/11/12 19:25
  */
@@ -19,24 +30,47 @@ public class Client {
         try {
             SocketChannel socketChannel = SocketChannel.open();
             //设置为非阻塞模式
-//            socketChannel.configureBlocking(false);
-            socketChannel.connect(new InetSocketAddress("127.0.0.1", 8080));
-            while (true) {
-//                for (int i = 0; i < 10; i++) {
-                String msg = "hello server, this is client " + UUID.randomUUID().toString();
-                byte[] bytes = msg.getBytes();
-                ByteBuffer buffer = byte2Byffer(bytes);
-                socketChannel.write(buffer);
-//                }
-                ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-                int read = 0;
-//                    while (1024 <= (read = socketChannel.read(byteBuffer))) {
-                byte[] bytes0 = bytebuffer2ByteArray(byteBuffer);
-                System.out.println("read=" + read);
-                System.out.println("content：" + new String(bytes0));
-//                    }
-//                }
-            }
+            //socketChannel.configureBlocking(false);
+            socketChannel.connect(new InetSocketAddress("127.0.0.1", 8090));
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int i = 0;
+                    Scanner scanner = new Scanner(System.in);
+                    while (true) {
+                        System.out.print("Please Input: ");
+                        String msg = scanner.nextLine();
+                        byte[] bytes = msg.getBytes();
+                        ByteBuffer buffer = byte2Byffer(bytes);
+                        try {
+                            socketChannel.write(buffer);
+//                            socketChannel.shutdownOutput();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                    }
+                }
+            }).start();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                        //阻塞，直到有数据可读或者到达流的末尾
+                        int read = 0;
+                        try {
+                            read = socketChannel.read(byteBuffer);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            break;
+                        }
+                        byte[] bytes0 = bytebuffer2ByteArray(byteBuffer);
+                        System.out.println("read=" + read);
+                        System.out.println("content：" + new String(bytes0));
+                    }
+                }
+            }).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/com/example/nio/channel/Service.java
+++ src/main/java/com/example/nio/channel/Service.java
@@ -20,24 +20,50 @@ public class Service {
             ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
             //todo ??
             //serverSocketChannel.bind(new InetSocketAddress(8080));
-            serverSocketChannel.socket().bind(new InetSocketAddress(8080));
+            serverSocketChannel.socket().bind(new InetSocketAddress(8090));
             //非阻塞模式
-//            serverSocketChannel.configureBlocking(false);
+            //serverSocketChannel.configureBlocking(false);
             while (true) {
+                System.out.println("wait...");
                 SocketChannel accept = serverSocketChannel.accept();
                 //非阻塞模式打开后，serverSocketChannel.accept() 无论有没有连接都会立即返回，即 accept 有可能 = null;
-                System.out.println("accept");
-                ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-                int read = accept.read(byteBuffer);
-                byte[] bytes = bytebuffer2ByteArray(byteBuffer);
-                System.out.println("read=" + read);
-                String msg0 = new String(bytes);
-                System.out.println("content：" + msg0);
+                if (null != accept) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println("accept");
+                            while (true) {
+                                try {
+                                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                                    int read = accept.read(byteBuffer);
+                                    byte[] bytes = bytebuffer2ByteArray(byteBuffer);
+                                    System.out.println("read=" + read);
+                                    String msg0 = new String(bytes);
+                                    if("bye".equals(msg0)){
+                                        break;
+                                    }
+                                    System.out.println("content：" + msg0);
+                                    String msg = "hello client, this is server " + msg0;
+                                    byte[] bytes0 = msg.getBytes();
+                                    ByteBuffer buffer = byte2Byffer(bytes0);
+                                    accept.write(buffer);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                    break;
+                                }
+                            }
+                            System.out.println("结束");
+//                            try {
+////                                accept.close();
+//                            } catch (IOException e) {
+//                                e.printStackTrace();
+//                            }
+                        }
+                    }).start();
 
-                String msg = "hello client, this is server " + msg0;
-                byte[] bytes0 = msg.getBytes();
-                ByteBuffer buffer = byte2Byffer(bytes0);
-                accept.write(buffer);
+                } else {
+                    System.out.println("No accept");
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
