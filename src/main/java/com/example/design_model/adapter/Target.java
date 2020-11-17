a1eeccf868c84a35b603bffdadf32912fbe03f5e
xiankun.jiang <xiankun.jiang@aorise.org>
2020-11-17 15:13:54 +0800

    设计模式--适配器模式

---
+++ src/main/java/com/example/design_model/adapter/Target.java
@@ -0,0 +1,10 @@
+package com.example.design_model.adapter;
+
+/**
+ * @Description:
+ * @Author: xiankun.jiang
+ * @Date: 2020/11/17 15:09
+ */
+public interface Target {
+    public void request();
+}
