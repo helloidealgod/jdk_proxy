c092e3c5ddfd96206f512a2a2e0a8e903e2e9921
helloidealgod <443604325@qq.com>
2022-12-05 21:52:02 +0800

    cc 单词分割

--- src/main/java/com/example/compile/Main.java
+++ src/main/java/com/example/compile/Main.java
@@ -10,6 +10,7 @@ public class Main {
             int read;
             char c;
             StringBuilder token = new StringBuilder("");
+            int[][] stateMap = new int[2][2];
             int status = 0;
             // 0-9 a-z A-Z
             // +-*/ % = ! <> [] {} . & () ; , \ ' "
@@ -19,7 +20,7 @@ public class Main {
                 c = (char) read;
                 if ('a' <= c && 'z' >= c || 'A' <= c && 'Z' >= c || '0' <= c && '9' >= c || '_' == c || '#' == c || '.' == c) {
                     token.append(c);
-                } else if ('(' == c || ')' == c || '>' == c || '<' == c || '{' == c || '}' == c || ';' == c || ',' == c) {
+                } else if ('(' == c || ')' == c || '>' == c || '<' == c || '{' == c || '}' == c || ';' == c || ',' == c || '[' == c || ']' == c || '~' == c) {
                     if (0 < token.length() && 0 == status) {
                         pr.unread(read);
                     } else {
@@ -40,13 +41,94 @@ public class Main {
                         status = 0;
                     }
                     token.append(c);
-                } else if (' ' == c) {
+                } else if (' ' == c || '\n' == c) {
                     if (0 == status) {
                         if (0 < token.length()) {
                             tokenProcess(token);
                         }
                     } else {
                         token.append(c);
+                    }
+                } else if ('+' ==c ){
+                    if(0 == status){
+                        status = 3;
+                        token.append(c);
+                    }else if(3 == status){
+                        token.append(c);
+                        tokenProcess(token);
+                        status = 0;
+                    }
+                }else if ('-' ==c ){
+                    if(0 == status){
+                        status = 4;
+                        token.append(c);
+                    }else if(4 == status){
+                        token.append(c);
+                        tokenProcess(token);
+                        status = 0;
+                    }
+                }else if ('*' ==c ){
+                    if(0 == status){
+                        status = 5;
+                        token.append(c);
+                    }else if(5 == status){
+                        token.append(c);
+                        tokenProcess(token);
+                        status = 0;
+                    }
+                }else if ('/' ==c ){
+                    if(0 == status){
+                        status = 6;
+                        token.append(c);
+                    }else if(6 == status){
+                        token.append(c);
+                        tokenProcess(token);
+                        status = 0;
+                    }
+                }else if ('%' ==c ){
+                    if(0 == status){
+                        status = 7;
+                        token.append(c);
+                    }else if(7 == status){
+                        token.append(c);
+                        tokenProcess(token);
+                        status = 0;
+                    }
+                }else if ('&' ==c ){
+                    if(0 == status){
+                        status = 8;
+                        token.append(c);
+                    }else if(8 == status){
+                        token.append(c);
+                        tokenProcess(token);
+                        status = 0;
+                    }
+                }else if ('|' ==c ){
+                    if(0 == status){
+                        status = 9;
+                        token.append(c);
+                    }else if(9 == status){
+                        token.append(c);
+                        tokenProcess(token);
+                        status = 0;
+                    }
+                }else if ('!' ==c ){
+                    if(0 == status){
+                        status = 10;
+                        token.append(c);
+                    }else if(10 == status){
+                        pr.unread(c);
+                        tokenProcess(token);
+                        status = 0;
+                    }
+                }else if ('=' ==c ){
+                    if(0 == status || 3 <= status && 10 >= status){
+                        status = 11;
+                        token.append(c);
+                    }else if(11 == status){
+                        token.append(c);
+                        tokenProcess(token);
+                        status = 0;
                     }
                 }
             }
