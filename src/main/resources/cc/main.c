1655bccea190707a8b66715157beedf4446064ee
helloidealgod <443604325@qq.com>
2022-12-07 22:25:09 +0800

    变量声明 基础类型声明初始化，数组，指针声明

--- src/main/resources/cc/main.c
+++ src/main/resources/cc/main.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
-# include "hello.h"
+#include "hello.h"
 
-int int1,int2;
-float float1,float2;
-char char1,char2;
+int int1;
+int int2,int3,int4;
+int int5=1;
+int int6=2,int7=7;
+int int8[];
+int int9[9];
+int inta[1][2];
+int inta3[1][2][3];
+int *intb;
+int **intc;
+int ***intd;
 
-int main(){
-	int int3 = 3;
-	float float3 = 9.8f;
-	char char3 = 'a';
-	int[2] intarray2;
-	int[] intarray3={1,2,3};
-	printf("hello world!");
-	int3++;
-	int3--;
-	int3+=1;
-	int3-=1;
-	int3*=1;
-	int3/=1;
-	int3%=1;
-	if(int3 == 2 && int2 != 2){
-
-	}
-	int2&=2;
-	int2|=1;
-	return 0;
-}
-
+float float1;
+float float2,float3,float4;
+float float5=1;
+float float6=2,float7=7;
+float float8[];
+float float9[9];
+float floata[1][2];
+float floata3[1][2][3];
+float *floatb;
+float **floatc;
+float ***floatd;
