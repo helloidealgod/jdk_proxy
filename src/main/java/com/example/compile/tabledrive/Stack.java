185555b5f53b9884a0ded81032aff939d1435e98
helloidealgod <443604325@qq.com>
2023-06-23 01:27:06 +0800

    update

--- src/main/java/com/example/compile/tabledrive/Stack.java
+++ src/main/java/com/example/compile/tabledrive/Stack.java
@@ -5,13 +5,10 @@ import java.util.List;
 
 public class Stack {
     public List<String> toKenList = new ArrayList<>();
-    public List<Integer> typeList = new ArrayList<>();
     public List<Integer> statusList = new ArrayList<>();
-    public List<String> descList = new ArrayList<>();
     public int topIndex = -1;
     public int index = 0;
     public int statusTopIndex = -1;
-    public int statusIndex = 0;
 
     public void push(String token) {
         toKenList.add(token);
@@ -33,19 +30,6 @@ public class Stack {
         return token;
     }
 
-    public void push(String token, Integer type) {
-        toKenList.add(token);
-        typeList.add(type);
-        topIndex++;
-    }
-
-    public void push(String token, Integer type, String desc) {
-        toKenList.add(token);
-        typeList.add(type);
-        descList.add(desc);
-        topIndex++;
-    }
-
     public String pop() {
         String token = null;
         if (0 <= topIndex) {
@@ -56,46 +40,10 @@ public class Stack {
         return token;
     }
 
-    public String peekToken() {
-        String token = null;
-        if (0 <= topIndex) {
-            token = toKenList.get(topIndex);
-        }
-        return token;
-    }
-
-    public String peekDesc() {
-        String token = null;
-        if (0 <= topIndex) {
-            token = descList.get(topIndex);
-        }
-        return token;
-    }
-
-    public Integer peekType() {
-        Integer token = null;
-        if (0 <= topIndex) {
-            token = typeList.get(topIndex);
-        }
-        return token;
-    }
-
     public void empty() {
         toKenList = new ArrayList<>();
-        typeList = new ArrayList<>();
-        descList = new ArrayList<>();
         topIndex = -1;
         index = 0;
-    }
-
-    public void print() {
-        if (0 < toKenList.size()) {
-            System.out.println("============start============");
-            for (int i = 0; i < toKenList.size(); i++) {
-                System.out.println(typeList.get(i) + " " + toKenList.get(i));
-            }
-            System.out.println("=============end=============");
-        }
     }
 
     public void printToken() {
@@ -112,19 +60,9 @@ public class Stack {
         if (0 < statusList.size()) {
             System.out.print("状态栈内容：");
             for (int i = 0; i < statusList.size(); i++) {
-                System.out.print(statusList.get(i)+" ");
+                System.out.print(statusList.get(i) + " ");
             }
             System.out.println("");
-        }
-    }
-
-    public void printDesc() {
-        if (0 < toKenList.size()) {
-            System.out.println("============start============");
-            for (int i = 0; i < toKenList.size(); i++) {
-                System.out.println(typeList.get(i) + " " + toKenList.get(i) + " " + descList.get(i));
-            }
-            System.out.println("=============end=============");
         }
     }
 
