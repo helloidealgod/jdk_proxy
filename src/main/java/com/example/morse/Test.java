46bf70af81f01a0a10f2d7bbbee79f8b02696b22
helloidealgod <443604325@qq.com>
2026-06-12 09:16:07 +0800

    摩尔斯码

--- src/main/java/com/example/morse/Test.java
+++ src/main/java/com/example/morse/Test.java
@@ -5,55 +5,55 @@ public class Test {
     public final String[] numberCode = {"-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
 
     public static final byte[] morse_table = {
-            (byte) 0x48,
-            (byte) 0x90,
-            (byte) 0x94,
-            (byte) 0x70,
-            (byte) 0x20,
-            (byte) 0x84,
-            (byte) 0x78,
-            (byte) 0x80,
-            (byte) 0x40,
-            (byte) 0x8E,
-            (byte) 0x74,
-            (byte) 0x88,
-            (byte) 0x58,
-            (byte) 0x50,
-            (byte) 0x7C,
-            (byte) 0x8C,
-            (byte) 0x9A,
-            (byte) 0x68,
-            (byte) 0x60,
-            (byte) 0x30,
-            (byte) 0x64,
-            (byte) 0x82,
-            (byte) 0x6C,
-            (byte) 0x92,
-            (byte) 0x96,
-            (byte) 0x98,
-            (byte) 0xBF,
-            (byte) 0xAF,
-            (byte) 0xA7,
-            (byte) 0xA3,
-            (byte) 0xA1,
-            (byte) 0xA0,
-            (byte) 0xB0,
-            (byte) 0xB8,
-            (byte) 0xBC,
-            (byte) 0xBE
+            (byte) 0x48, // A
+            (byte) 0x90, // B
+            (byte) 0x94, // C
+            (byte) 0x70, // D
+            (byte) 0x20, // E
+            (byte) 0x84, // F
+            (byte) 0x78, // G
+            (byte) 0x80, // H
+            (byte) 0x40, // I
+            (byte) 0x8E, // J
+            (byte) 0x74, // K
+            (byte) 0x88, // L
+            (byte) 0x58, // M
+            (byte) 0x50, // N
+            (byte) 0x7C, // O
+            (byte) 0x8C, // P
+            (byte) 0x9A, // Q
+            (byte) 0x68, // R
+            (byte) 0x60, // S
+            (byte) 0x30, // T
+            (byte) 0x64, // U
+            (byte) 0x82, // V
+            (byte) 0x6C, // W
+            (byte) 0x92, // X
+            (byte) 0x96, // Y
+            (byte) 0x98, // Z
+            (byte) 0xBF, // 0
+            (byte) 0xAF, // 1
+            (byte) 0xA7, // 2
+            (byte) 0xA3, // 3
+            (byte) 0xA1, // 4
+            (byte) 0xA0, // 5
+            (byte) 0xB0, // 6
+            (byte) 0xB8, // 7
+            (byte) 0xBC, // 8
+            (byte) 0xBE  // 9
     };
 
     public static final byte[] short_morse_table = {
-            (byte) 0x30,
-            (byte) 0x48,
-            (byte) 0x64,
-            (byte) 0x82,
-            (byte) 0xA1,
-            (byte) 0x20,
-            (byte) 0xB0,
-            (byte) 0x90,
-            (byte) 0x70,
-            (byte) 0x50
+            (byte) 0x30, // 0
+            (byte) 0x48, // 1
+            (byte) 0x64, // 2
+            (byte) 0x82, // 3
+            (byte) 0xA1, // 4
+            (byte) 0x20, // 5
+            (byte) 0xB0, // 6
+            (byte) 0x90, // 7
+            (byte) 0x70, // 8
+            (byte) 0x50  // 9
     };
 
     public static void main(String[] args) {
@@ -68,7 +68,10 @@ public class Test {
             System.out.print(" " + String.format("%3s", Integer.toBinaryString(len)).replace(' ', '0'));
 
             int code = (b & 0x1F) << 3;
-            System.out.print(" " + String.format("%5s", Integer.toBinaryString(code)).replace(' ', '0'));
+            System.out.print(" " + String.format("%8s", Integer.toBinaryString(code)).replace(' ', '0'));
+            System.out.print(" ");
+
+            System.out.print(" " + String.format("%5s", Integer.toBinaryString((b & 0x1F))).replace(' ', '0'));
             System.out.print(" ");
 
             for (; len > 0; len--) {
